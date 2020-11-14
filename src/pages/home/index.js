1522d195b1860b97ce7a03af57b0e9713022c400
igortaquary <igortaquary@gmail.com>
2020-11-14 15:34:46 -0300

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './style.css';
-//import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import logo from '../../assets/logoCriado.png';
-import { FaWhatsapp } from 'react-icons/fa'
+import { FaWhatsapp, FaWpforms } from 'react-icons/fa'
 
 export default function Home() {
   return (
@@ -11,13 +11,19 @@ export default function Home() {
         <div ><img src={logo} alt="logo" className='navLogo' /></div>
         <ul>
           <li>
-            Nossos Serviços
+            <Link to="servicos" smooth={true} duration={500} offset={-70}>
+              Nossos Serviços
+            </Link>
           </li>
           <li>
-            Contato
+            <Link to="contato" smooth={true} duration={500} offset={-70}>
+              Contato
+            </Link>
           </li>
           <li>
-            Quem Somos
+            <Link to="bio" smooth={true} duration={500} offset={-70}>
+              Quem Somos
+            </Link>
           </li>
         </ul>
       </nav>
@@ -26,21 +32,50 @@ export default function Home() {
         <br />
         Fale conosco via
         <a href="https://api.whatsapp.com/send?phone=5561994329979" target="blank">
-          <div className='wppButton'><FaWhatsapp /> Whatsapp</div>
+          <div className='wppButton'>
+            <FaWhatsapp /> Whatsapp
+          </div>
         </a>
         ou faça um
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSchCsfiKJJl3A-krDW3trxSg0_xEUQ-e-nr7WJlK_HAUp4JUQ/viewform?usp=sf_link"
           target="blank">
           <div className="orcamentoButton">
-            Orçamento
+            <FaWpforms /> Orçamento
           </div>
         </a>
       </header>
       <main>
-        <div>
-
+        <div className='segmento' name='bio'>
+          <h1>Quem Somos</h1>
+          <p>
+            A <b>Service</b> é uma empresa especializada em serviços de pintura, manutenção, gesso e acabamento em geral.<br />
+            Com mais de 20 anos de experiência e com diversos clientes atendidos em todo <b>Distrito Federal</b>, somos referência na prestação de serviços de alta qualidade.
+          </p>
+        </div>
+        <hr />
+        <div className='segmento' name='servicos'>
+          <h1>Nossos Serviços</h1>
+          <p>
+            A <b>Service</b> desempenha todos os serviços, seja eles pequeno ou grande porte como reformas estruturais ou estéticas.
+            <ul>
+              <li>
+                <h3>Pintura</h3>
+                <p>
+                  Realizamos pintura predial, comercial e residencial.
+                  Tratamos fissuras, trincas e infiltrações com tintas e produtos impermeabilizantes.
+                </p>
+              </li>
+            </ul>
+          </p>
+        </div>
+        <hr />
+        <div className='segmento' name='contato'>
+          <h1>Contato</h1>
         </div>
       </main>
+      <footer>
+
+      </footer>
     </body>
   )
 }
